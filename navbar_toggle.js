80e181f2de756a399fb9bac586e9acf23cbe4b51
shira1008 <shaharshira1008@gmail.com>
2023-08-29 18:25:29 +0300

    added fix to nav toggle

--- navbar_toggle.js
+++ navbar_toggle.js
@@ -4,3 +4,9 @@ const linkContainer = document.querySelector(".nav-links");
 navBtn.addEventListener("click", () => {
   linkContainer.classList.toggle("show-links");
 });
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 975) {
+    linkContainer.classList.remove("show-links");
+  }
+});
